82073d46feba7f6bfaa10f6b974e74ee0ac1bc84
imjamnotjelly <81252683+imjamnotjelly@users.noreply.github.com>
2023-05-11 14:59:58 -0400

    Update shell_hack.py

--- src/shell_hack.py
+++ src/shell_hack.py
@@ -1,16 +1,23 @@
+# Usage
+# Just run and enjoy!
+
+# Settings
+# ---------------
+UNAME = ""
+STARTING_DIR = ""
+HOSTNAME = "codehs"
+NAME_COL = ""
+DIR_COL = ""
+# ---------------
+
+
 import os
 import readline
 from subprocess import run
 from socket import gethostname
 from getpass import getuser
-
-
-# Settings
-# ---------------
-UNAME = "imjamnotjelly"
-STARTING_DIR = ""
-HOSTNAME = "codehs"
-# ---------------
+from lib_hack import install_lib
+qs = install_lib("quick_styles")
 
 def set_default(var, default):
     return var or default
@@ -21,6 +28,8 @@ def dash_to_space(text):
 UNAME = dash_to_space(set_default(UNAME, getuser()))
 STARTING_DIR = set_default(STARTING_DIR, ".")
 HOSTNAME = dash_to_space(set_default(HOSTNAME, gethostname()))
+NAME_COL = set_default(NAME_COL, "green")
+DIR_COL = set_default(DIR_COL, "blue")
 
 FILE_ERRS = (
     PermissionError,
@@ -31,7 +40,11 @@ run(f"cd {STARTING_DIR}", shell=True)
 os.chdir(STARTING_DIR)
 run("clear", shell=True)
 while True:
-    cmd = input(f"{UNAME}@{HOSTNAME}:{os.getcwd() if os.getcwd() != '/' else '~'} $ ")
+    cmd = input(
+        qs.style_string(f"{UNAME}@{HOSTNAME}:", color=NAME_COL) + 
+        qs.style_string(os.getcwd() if os.getcwd() != '/' else '~', color=DIR_COL) + 
+        " $ "
+    )
     
     run(cmd, shell=True)
     
